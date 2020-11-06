88952576c1ded09171ec9778dec37d39274811ec
Kyle Graas <kgraas09@gmail.com>
2020-11-06 13:59:05 -0700

    Edited server.js file

---
+++ frontend/build/precache-manifest.64bc2e03343f7c1b6380c92ddeaab8c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4eaed0bfd0c5c8a930abbeb5ceda7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a9a7a11e7dea90ae1a7",
+    "url": "/static/css/main.1081b816.chunk.css"
+  },
+  {
+    "revision": "5241aca127d4bf5b3e65",
+    "url": "/static/js/2.178411a1.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.178411a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a9a7a11e7dea90ae1a7",
+    "url": "/static/js/main.75a3c9ea.chunk.js"
+  },
+  {
+    "revision": "6eface667ac3aa6edc99",
+    "url": "/static/js/runtime-main.d6aabd63.js"
+  }
+]);
